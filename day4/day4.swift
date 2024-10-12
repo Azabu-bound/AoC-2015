d5069dc9dea16e0a8a18dd06cc3347cb9f0bcf1d
Azabu-bound <tylerjbailey95@gmail.com>
2024-10-12 10:07:26 -0400

    Day 4 part 1 solved

---
+++ day4/day4.swift
@@ -0,0 +1,21 @@
+import CryptoKit
+
+func findLowestNumber(secretKey: String) -> Int {
+  var number = 1
+
+  while true {
+    let input = secretKey + String(number)
+
+    // compute MD5 hash
+    let digest = Insecure.MD5.hash(data: input.data(using: .utf8)!)
+    let md5String = digest.map { String(format: "%02hhx", $0) }.joined()
+
+    // check if the hash begins with 5 zeros
+    if md5String.hasPrefix("00000") { return number }
+    number += 1
+  }
+}
+
+let secretKey = "ckczppom"
+let result = findLowestNumber(secretKey: secretKey)
+print("The lowest number is: \(result)")
